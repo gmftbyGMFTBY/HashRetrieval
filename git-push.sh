83096ec39dae21f7a802d8bf15ee8098152712e5
gmftbyGMFTBY <18811371908@163.com>
2020-11-10 21:25:13 +0800

    Tue 10 Nov 2020 09:25:13 PM CST init

---
+++ git-push.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+dating=`date`
+git status
+git add .
+git commit -m "$dating $1"
+git push origin master
+git log 
---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bsah
+
+# ./run.sh <mode> <dataset> <cuda>
+mode=$1    # train, test
+dataset=$2    # E-Commerce, Douban, Zh50w (or LCCC)
+cuda=$3
+
+mkdir ckpt
+mkdir rest
+mkdir bak
